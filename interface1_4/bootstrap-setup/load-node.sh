1277c5a8b8d90a16fb4d3d229eabb4785344ffd6
visser-glr <visser@glr.nl>
2025-05-19 12:11:34 +0200

    update
    - settings json simplified
    - added interface 1_4

---
+++ interface1_4/bootstrap-setup/load-node.sh
@@ -0,0 +1,14 @@
+# bootstrap met npm gebruiken
+
+# create directory 
+mkdir <dir>
+# cd in directory
+cd <dir>
+# maak package.js met standaard waarden
+npm init -y
+# installeer bootstrap module@versie
+npm install bootstrap@5.3.6
+
+
+
+
